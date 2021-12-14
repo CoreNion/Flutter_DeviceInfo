22372c7d9c05e06b4533f28b48dbacbcdeb95f92
CoreNion <coreinion@gmail.com>
2021-12-14 23:36:41 +0900

    [iOS] MGCopyAnswerから必要な値が取れるように！
    Flutterでも出来て良かった...
    Flutterがネイティブの型に対応してないので処理は若干遠回りです

--- lib/getinfo_platform/ios.dart
+++ lib/getinfo_platform/ios.dart
@@ -1,6 +1,11 @@
+import 'dart:ffi';
+import 'dart:typed_data';
+import 'package:ffi/ffi.dart';
+import 'package:propertylistserialization/propertylistserialization.dart';
+
 class GetiDeviceInfo {
   static String modelName() {
-    return "iOS Model Name Dummy";
+    return chipID() + " Device";
   }
 
   static String modelNumber() {
@@ -40,7 +45,7 @@ class GetiDeviceInfo {
   }
 
   static String chipID() {
-    return "Chip ID Dummy";
+    return _getMGCopyAnswerValue("HardwarePlatform");
   }
 
   static String totalMemoryMB() {
@@ -49,5 +54,88 @@ class GetiDeviceInfo {
 
   static String gpuName() {
     return "GPU Name Dummy";
+  }
+
+  static String _getMGCopyAnswerValue(String property) {
+    // String to C String
+    Pointer<Utf8> hardwarePlatform = property.toNativeUtf8();
+    // C String to CFString, after doing MGCopyAnswer
+    Pointer<IntPtr> answer =
+        _MGCopyAnswer(_CFStringCreateWithCString(hardwarePlatform, 134217984));
+    // CFPropertyListRef(MGCopyAnswer's return) to CFDataRef(Binary Plist type)
+    Pointer<IntPtr> answerCFData = _CFPropertyListCreateData(answer);
+    // CFDataRef to UInt8 to Unint8List (dart becomes a readable type.)
+    Uint8List answerUint8ListData = _CFDataGetBytePtr(answerCFData)
+        .asTypedList(_CFDataGetLength(answerCFData));
+    // Unint8List to ByteData
+    ByteData answerByteData = ByteData.sublistView(answerUint8ListData);
+    // At last. After serialization binary plist, we can get Chip ID.
+    return PropertyListSerialization.propertyListWithData(answerByteData)
+        as String;
+  }
+
+  static Pointer<IntPtr> _MGCopyAnswer(Pointer<IntPtr> property) {
+    String dylibPath = "/usr/lib/libMobileGestalt.dylib";
+    final libMobileGestalt = DynamicLibrary.open(dylibPath);
+    final MGCopyAnswer = libMobileGestalt
+        .lookup<NativeFunction<Pointer<IntPtr> Function(Pointer<IntPtr>)>>(
+            'MGCopyAnswer');
+    final mgCopyAnswer =
+        MGCopyAnswer.asFunction<Pointer<IntPtr> Function(Pointer<IntPtr>)>();
+    return mgCopyAnswer(property);
+  }
+
+  static Pointer<IntPtr> _CFStringCreateWithCString(
+      Pointer<Utf8> cStr, int count) {
+    String cfPath =
+        "/System/Library/Frameworks/CoreFoundation.framework/CoreFoundation";
+    final cF = DynamicLibrary.open(cfPath);
+    final CFStringCreateWithCString = cF.lookup<
+        NativeFunction<
+            Pointer<IntPtr> Function(Pointer<IntPtr>, Pointer<Utf8>,
+                Uint32)>>('CFStringCreateWithCString');
+    final cFStringCreateWithCString = CFStringCreateWithCString.asFunction<
+        Pointer<IntPtr> Function(Pointer<IntPtr>, Pointer<Utf8>, int)>();
+    return cFStringCreateWithCString(nullptr, cStr, count);
+  }
+
+  static Pointer<IntPtr> _CFPropertyListCreateData(
+      Pointer<IntPtr> cfPropertyList) {
+    String cfPath =
+        "/System/Library/Frameworks/CoreFoundation.framework/CoreFoundation";
+    final cF = DynamicLibrary.open(cfPath);
+    final CFPropertyListCreateData = cF.lookup<
+        NativeFunction<
+            Pointer<IntPtr> Function(Pointer<IntPtr>, Pointer<IntPtr>, Uint32,
+                Pointer<Uint64>, Pointer<IntPtr>)>>('CFPropertyListCreateData');
+    final cfPropertyListCreateData = CFPropertyListCreateData.asFunction<
+        Pointer<IntPtr> Function(Pointer<IntPtr>, Pointer<IntPtr>, int,
+            Pointer<Uint64>, Pointer<IntPtr>)>();
+    return cfPropertyListCreateData(
+        nullptr, cfPropertyList, 200, nullptr, nullptr);
+  }
+
+  static Pointer<Uint8> _CFDataGetBytePtr(Pointer<IntPtr> cfData) {
+    String cfPath =
+        "/System/Library/Frameworks/CoreFoundation.framework/CoreFoundation";
+    final cF = DynamicLibrary.open(cfPath);
+    final CFDataGetBytePtr =
+        cF.lookup<NativeFunction<Pointer<Uint8> Function(Pointer<IntPtr>)>>(
+            'CFDataGetBytePtr');
+    final cfDataGetBytePtr =
+        CFDataGetBytePtr.asFunction<Pointer<Uint8> Function(Pointer<IntPtr>)>();
+    return cfDataGetBytePtr(cfData);
+  }
+
+  static int _CFDataGetLength(Pointer<IntPtr> cfData) {
+    String cfPath =
+        "/System/Library/Frameworks/CoreFoundation.framework/CoreFoundation";
+    final cF = DynamicLibrary.open(cfPath);
+    final CFDataGetLength =
+        cF.lookup<NativeFunction<Int32 Function(Pointer<IntPtr>)>>(
+            'CFDataGetLength');
+    final cfDataGetLength =
+        CFDataGetLength.asFunction<int Function(Pointer<IntPtr>)>();
+    return cfDataGetLength(cfData);
   }
 }
