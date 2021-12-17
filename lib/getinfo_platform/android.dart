e620f95c13d6f5631c15c77eb1c46f8a6cca5854
CoreNion <coreinion@gmail.com>
2021-12-17 16:23:32 +0900

    [Android] システムプロパティから必要な値を取得できるように！
    Androidのlibc.soの__system_property_get()を利用

--- lib/getinfo_platform/android.dart
+++ lib/getinfo_platform/android.dart
@@ -1,14 +1,27 @@
+import 'dart:ffi';
+import 'dart:io';
+import 'package:ffi/ffi.dart';
+
 class GetDroidDeviceInfo {
   static String modelName() {
-    return "Android Model Name Dummy";
+    return systemPropertyGet("ro.product.manufacturer") +
+        " " +
+        systemPropertyGet("ro.product.model");
   }
 
   static String modelNumber() {
-    return "Model Number Dummy";
+    return systemPropertyGet("ro.product.name");
   }
 
   static String osVersion() {
-    return "OS Version Dummy";
+    return "Android " +
+        systemPropertyGet("ro.build.version.release") +
+        " (API: " +
+        systemPropertyGet("ro.build.version.sdk") +
+        ") " +
+        systemPropertyGet("ro.build.version.incremental") +
+        "\n" +
+        systemPropertyGet("ro.build.display.id");
   }
 
   static String widthResolution() {
@@ -20,7 +33,7 @@ class GetDroidDeviceInfo {
   }
 
   static String boardName() {
-    return "Board Name Dummy";
+    return systemPropertyGet("ro.product.board");
   }
 
   static String kernelVersion() {
@@ -32,7 +45,7 @@ class GetDroidDeviceInfo {
   }
 
   static String cpuArch() {
-    return "CPU Arch Dummy";
+    return systemPropertyGet("ro.product.cpu.abi");
   }
 
   static String cpuCores() {
@@ -49,5 +62,39 @@ class GetDroidDeviceInfo {
 
   static String gpuName() {
     return "GPU Name Dummy";
+  }
+
+  /// Get system property(build.prop)'s value.
+  ///
+  /// (Using __system_property_get() from android's libc.so)
+  static String systemPropertyGet(String name) {
+    // Check having 64bit library.
+    String soPath = "";
+    if (File("/system/lib64/libc.so").existsSync()) {
+      soPath = "/system/lib64/libc.so";
+    } else {
+      soPath = "/system/lib/libc.so";
+    }
+
+    final libc = DynamicLibrary.open(soPath);
+
+    // int __system_property_get(const char* __name, char* __value);
+    // header: https://android.googlesource.com/platform/bionic/+/refs/tags/android-12.0.0_r21/libc/include/sys/system_properties.h
+    // While __system_property_get is deprecated, but using __system_property_read_callback is very difficult.
+    final __system_property_get = libc
+        .lookup<NativeFunction<Int32 Function(Pointer<Utf8>, Pointer<Int8>)>>(
+            '__system_property_get');
+    final system_property_get = __system_property_get
+        .asFunction<int Function(Pointer<Utf8>, Pointer<Int8>)>();
+
+    // Set char* __value
+    final pointer = calloc<Int8>();
+    // 92 is PROP_VALUE_MAX's value.
+    pointer.value = 92;
+
+    system_property_get(name.toNativeUtf8(), pointer);
+    String value = pointer.cast<Utf8>().toDartString();
+    calloc.free(pointer);
+    return value;
   }
 }
